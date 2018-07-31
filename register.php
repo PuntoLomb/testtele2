babd5ca429dd05d0ac962be4ee315e9cc18f4682
PuntoLomb <41970480+PuntoLomb@users.noreply.github.com>
2018-07-31 21:25:56 +0200

    test2

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://testpunto.herokuapp.com/execute.php';
+$BOT_TOKEN = '686085780:AAGRNC4PwNFnOy_fJOSRxW9cZXyZ9zHnBqY';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
